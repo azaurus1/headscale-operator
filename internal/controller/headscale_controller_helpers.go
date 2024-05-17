1627852771532d31a209a9def5ebe50e473c037a
Liam Aikin <liamaikin@gmail.com>
2024-05-17 15:39:18 +1000

    added creating svcs

--- internal/controller/headscale_controller_helpers.go
+++ internal/controller/headscale_controller_helpers.go
@@ -19,15 +19,48 @@ func (r *HeadscaleReconciler) EnsureHeadscaleServer(ctx context.Context, server 
 
 	// headscaleServer := &apiv1.Pod{}
 	headscaleDeployment := &appsv1.Deployment{}
+	headscaleService := &apiv1.Service{}
 
 	// create the headscale deployment
 	err := r.EnsureHeadscaleDeployment(ctx, headscaleDeployment, name, version, configMapName)
 	if err != nil {
-		log.Error(err, "unabled to ensure headscale deployment")
+		log.Error(err, "unable to ensure headscale deployment")
 		return err
 	} else {
 		// if the pod already exist, check for annotations, etc.
 		log.Info("Headscale server deployment exists")
+
+		if server.Annotations == nil {
+			server.Annotations = map[string]string{}
+		}
+
+		// define required annotations and their desired values
+		requiredAnnotations := map[string]string{
+			"managed-by": headscaleOperatorAnnotation,
+		}
+
+		// iterated over required annotations and update if necessary
+		for key, desiredValue := range requiredAnnotations {
+			existingValue, ok := server.Annotations[key]
+			if !ok || existingValue != desiredValue {
+				log.Info("Updating headscale annotaion..")
+				server.Annotations[key] = desiredValue
+				err := r.Update(ctx, server)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// create the headscale service
+	err = r.EnsureHeadscaleService(ctx, headscaleService, name, version, configMapName)
+	if err != nil {
+		log.Error(err, "unable to ensure headscale service")
+		return err
+	} else {
+		// if the service already exist, check for annotations, etc.
+		log.Info("Headscale server service exists")
 
 		if server.Annotations == nil {
 			server.Annotations = map[string]string{}
@@ -57,6 +90,7 @@ func (r *HeadscaleReconciler) EnsureHeadscaleServer(ctx context.Context, server 
 func (r *HeadscaleReconciler) DeleteExternalResources(ctx context.Context, server *headscalev1.Headscale) error {
 	log := log.FromContext(ctx)
 
+	// delete deployment
 	headscaleServer := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      server.Spec.Name,
@@ -67,6 +101,21 @@ func (r *HeadscaleReconciler) DeleteExternalResources(ctx context.Context, serve
 	err := r.Delete(ctx, headscaleServer)
 	if err != nil {
 		log.Error(err, "unable to delete headscale server deployment")
+		return err
+	}
+
+	headscaleService := &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "svc-" + server.Spec.Name,
+			Namespace: "default", // using default for now, TODO: Change to namespace defined in server
+		},
+	}
+
+	log.Info("deleting svc " + ("svc-" + server.Spec.Name))
+
+	err = r.Delete(ctx, headscaleService)
+	if err != nil {
+		log.Error(err, "unable to delete headscale server service")
 		return err
 	}
 
@@ -224,6 +273,56 @@ func (r *HeadscaleReconciler) EnsureHeadscaleDeployment(ctx context.Context, hea
 			err = r.Create(ctx, headscaleDeployment)
 			if err != nil {
 				log.Error(err, "error creating headscale deployment")
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (r *HeadscaleReconciler) EnsureHeadscaleService(ctx context.Context, headscaleService *apiv1.Service, name string, version string, configMapName string) error {
+	log := log.FromContext(ctx)
+	logStr := fmt.Sprintf("Attempting to get a service with name %s", "svc-"+name)
+	log.Info(logStr)
+
+	err := r.Get(ctx, client.ObjectKey{Name: "svc-" + name, Namespace: "default"}, headscaleService)
+	if err != nil {
+		// if the pod doesnt exist, create it
+		if apierrors.IsNotFound(err) {
+			log.Info("Creating Headscale Server Service")
+			headscaleService := &apiv1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc-" + name,
+					Namespace: "default", // using default for now, TODO: Change to namespace defined in server
+					Labels: map[string]string{
+						"app": "headscale",
+					},
+					Annotations: map[string]string{
+						"managed-by": headscaleOperatorAnnotation,
+					},
+				},
+				Spec: apiv1.ServiceSpec{
+					Ports: []apiv1.ServicePort{
+						{
+							Name:     "tcp-8080",
+							Port:     8080,
+							Protocol: apiv1.ProtocolTCP,
+						},
+						{
+							Name:     "tcp-9090",
+							Port:     9090,
+							Protocol: apiv1.ProtocolTCP,
+						},
+					},
+					Selector: map[string]string{
+						"app": name,
+					},
+				},
+			}
+			// attempt to create the server service
+			err = r.Create(ctx, headscaleService)
+			if err != nil {
+				log.Error(err, "error creating headscale service")
 				return err
 			}
 		}
